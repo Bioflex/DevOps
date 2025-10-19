96fd9007feb46bbb09fb702c8287d7b88a4d6a69
Bioflex <atharva.bhagubhai@gmail.com>
2025-10-20 04:41:44 +0530

    Added index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>DevOps Login Page</title>
+</head>
+<body>
+    <h2>CI/CD Deployed Login</h2>
+    <form action="login.php" method="post">
+        <label for="username">Username:</label><br>
+        <input type="text" id="username" name="username" value="user"><br>
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" name="password" value="pass"><br><br>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html> 
